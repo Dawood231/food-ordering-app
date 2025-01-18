7e287bd9fa57f127eac6c81bdfb83a816326c581
Muhammad Dawood Siddiqui <164146729+Dawood231@users.noreply.github.com>
2025-01-18 23:42:22 +0500

    .

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <footer className="bg-gray-950 pt-12">
       <div className="grid grid-cols-2 md:grid-cols-5 gap-10 mb-6 px-10 lg:px-48">
         <div className="col-span-full lg:col-span-1 lg:block">
-          <Link href="/" className='text-primary text-2xl font-josefin'>Pizza Fiesta</Link>
+          <Link href="/" className='text-primary text-2xl font-josefin'>Desi Food</Link>
           <div className="mt-6 flex gap-6">
             <TwitterIcon className={"w-6"} />
             <FaceBookIcon className={"w-6"} />
@@ -48,23 +48,23 @@ const Footer = () => {
             <ul className="space-y-2">
               <li className="inline-flex space-x-4">
                 <span><LocationIcon className={"w-6"} /></span>
-                <span>20 Graham Rd, Malvern WR14 2HL, United Kingdom</span>
+                <span>20 Graham Rd, Malvern WR14 2HL, pakistan </span>
               </li>
               <li className="inline-flex space-x-4">
                 <span><PhoneIcon className={"w-6"} /></span>
-                <span>+44 168 4892 229</span>
+                <span>+92 334 311 2920</span>
               </li>
               <li className="inline-flex space-x-4">
                 <span><MailIcon className={"w-6"} /></span>
-                <span>info@pizzafiesta.com</span>
+                <span>info@desifood.com</span>
               </li>
             </ul>
           </div>
         </div>
       </div>
 
-      <div className="pt-5 border-t border-gray-700 text-center">
-      <p className="mt-4 text-gray-400">Copyright &copy; {new Date().getFullYear()} All rights reserved</p>
+      <div className="pt-5 border-t border-gray-700 text-center"> 
+      <p className="mt-4 text-gray-400">Copyright &copy; {new Date().getFullYear()} All rights reserved by Muhammad Dawood</p>
       </div>
     </footer>
   )
